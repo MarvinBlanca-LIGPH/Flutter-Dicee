ceeba0543edca09732b1f979ba9e551f3e56fa57
MarvinBlanca-LIGPH <76429291+MarvinBlanca-LIGPH@users.noreply.github.com>
2021-06-18 15:30:57 +0800

    Convert to Provider and Consumer

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,63 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:provider/provider.dart';
 
 void main() {
   return runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red.shade900,
-        appBar: AppBar(
-          title: Text('Dicee'),
+    ChangeNotifierProvider<GenerateNumber>(
+      create: (_) => GenerateNumber(),
+      child: MaterialApp(
+        home: Scaffold(
           backgroundColor: Colors.red.shade900,
+          appBar: AppBar(
+            title: Text('Dicee'),
+            backgroundColor: Colors.red.shade900,
+          ),
+          body: DicePage(),
         ),
-        body: DicePage(),
       ),
     ),
   );
 }
 
-class DicePage extends StatefulWidget {
+class DicePage extends StatelessWidget {
   @override
-  _DicePageState createState() => _DicePageState();
+  Widget build(BuildContext context) {
+    return Consumer<GenerateNumber>(builder: (context, generateNumber, child) {
+      return Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: TextButton(
+                child: Image.asset('images/dice${generateNumber.leftDice}.png'),
+                onPressed: () {
+                  generateNumber.randomizeDice();
+                },
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                child:
+                    Image.asset('images/dice${generateNumber.rightDice}.png'),
+                onPressed: () {
+                  generateNumber.randomizeDice();
+                },
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
 }
 
-class _DicePageState extends State<DicePage> {
+class GenerateNumber extends ChangeNotifier {
   int leftDice = 1;
   int rightDice = 1;
 
   void randomizeDice() {
-    setState(() {
-      leftDice = Random().nextInt(6) + 1;
-      rightDice = Random().nextInt(6) + 1;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          Expanded(
-            child: TextButton(
-              child: Image.asset('images/dice$leftDice.png'),
-              onPressed: () {
-                randomizeDice();
-              },
-            ),
-          ),
-          Expanded(
-            child: TextButton(
-              child: Image.asset('images/dice$rightDice.png'),
-              onPressed: () {
-                randomizeDice();
-              },
-            ),
-          ),
-        ],
-      ),
-    );
+    leftDice = Random().nextInt(6) + 1;
+    rightDice = Random().nextInt(6) + 1;
+    notifyListeners();
   }
 }
